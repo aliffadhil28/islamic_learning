0e2468e02c19ad239fdbe41a6a42e848359612d6
aliffadhil28 <alif70662@gmail.com>
2023-12-17 07:39:09 +0700

    fix: auth page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
         </div>
     </nav>
 
-    <div class="container mt-5">
+    <div class="container my-5">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md-6 pt-5 pe-md-4">
