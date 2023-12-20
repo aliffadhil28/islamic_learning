af20788157cc7a404b8af86cf46ea7b2b9aab937
aliffadhil28 <alif70662@gmail.com>
2023-12-20 14:50:26 +0700

    fix: landing page navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,13 +30,8 @@
                         <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">Pilih Kelas</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                            <li><a class="dropdown-item" href="#">Kelas 5 SD</a></li>
+                            <li><a class="dropdown-item" href="#">Kelas 6 SD</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
